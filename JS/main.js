036f476480f5c67ddcd8048209872617508a2b0a
Usef12 <yassouf9988@gmail.com>
2018-10-18 14:16:22 -0400

    If

--- JS/main.js
+++ JS/main.js
@@ -2,4 +2,39 @@ function test1(){
 	
     var fName = prompt("Hey brother?");
 	var myColour = prompt("Pick a colour," + fName);
+
+	if(myColour == "red"){
+		console.log("ha. this guy likes red");
+	}
+	else if(myColour == "green"){
+        console.log("ah,green. He's alright.");		
+	}
+	else if(myColour == "yellow"){
+        console.log("get him something yellow.");	
+    }
+    else{ 	
+	    console.log("that's not a colour")
+	}
+}
+
+function test2(){
+	 var age=prompt("how old are you?")
+	 
+	 
+	
+	if(age < 14){
+		alert("That's not too old")
+		
+	}
+	else if(age == 14){
+		alert("14 is a good age")
+		
+	}
+	else if(age > 14){
+		alert("You're older than I thought")
+		
+	}
+	else{
+		alert("Not a valid age")
+	}
 }
